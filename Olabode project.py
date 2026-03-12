02c784d0d3da14bec6cf2e9e61c348f7b48219bf
moses aina <bodjoye@gmail.com>
2026-03-12 14:59:54 +0100

    deployment of files

---
+++ Olabode project.py
@@ -0,0 +1,19 @@
+report='education, examination,project'
+print(report)
+print(report.split())
+
+number=10,20,30
+print(number)
+print(number.extend([40]))
+date="10/10/2010"
+print(date)
+full_name='olabode aina'
+
+hospital management
+hospital='nuel hosipal'
+bed='20 beds'
+doctors='5 doctors'
+
+hospital.replace('nuel,', 'chinsa hospital')
+
+print(hospital)
---
+++ main.py
@@ -0,0 +1,16 @@
+
+text='algorithm'git
+print(text)
+print(text[3:7])
+print(text[4:8])
+
+read="road map"
+print(read[2:6])
+read='01.production.pdf'
+print(read)
+
+laptop_detail=Hp laptop,
+
+a b=""
+
+
